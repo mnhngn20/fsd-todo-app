7e9554be84ad3989e4788a0cfdc71d256d70c975
Minh Nguyen <minh.nguyen@enouvo.com>
2024-11-10 11:51:58 +0700

    configure router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
+import { createRouter, RouterProvider } from '@tanstack/react-router';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import { App } from './app/app';
+import { routeTree } from './app/generated/route-tree.gen';
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+const router = createRouter({ routeTree });
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
